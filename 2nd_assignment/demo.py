eb0f72ad1aba1c8fc6f09389005e0dd65bd92fe1
giorgosskl <sklavenitisgeorge21@gmail.com>
2025-05-09 23:35:46 +0300

    fancy readme and threshold plots

--- 2nd_assignment/demo.py
+++ 2nd_assignment/demo.py
@@ -8,11 +8,11 @@ from log_edge import log_edge
 
 plt.ioff()
 
-output_dir = "./output_plots2"
+output_dir = "./output_plots"
 os.makedirs(output_dir, exist_ok=True)
 
 # Load images
-filename = "images/europe_map.gif"
+filename = "images/basketball_large.png"
 img = Image.open(fp=filename)
 bw_img = img.convert("L")
 img_array = np.array(bw_img).astype(float) / 255.0
@@ -20,8 +20,8 @@ plt.imshow(img_array, cmap='gray')
 plt.title("Original Image")
 plt.axis('off')
 plt.savefig(os.path.join(output_dir, "1_Original Image.png"))
-plt.show()
 
+# #Test fir_conv
 # h = np.array([[1, 1],
 #               [1, 1]])
 # in_img_array = np.array([[1, 2, 3],
@@ -37,12 +37,27 @@ plt.show()
 # print("Output Origin:", origin)
 
 # Sobel edge detection
-thres = 0.5
-out_img_array = sobel_edge(img_array, thres)
-plt.imshow(out_img_array, cmap='gray')
-plt.title("Sobel Edge Detection")
-plt.axis('off')
-plt.savefig(os.path.join(output_dir, "2_Sobel_Edge_Detection.png"))
+thres = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+detected_points = []
+
+for i in thres:
+    out_img_array = sobel_edge(img_array, i)
+    num_edges = np.sum(out_img_array) # Count the number of edge pixels
+    detected_points.append(num_edges)
+
+    # Plotting the edge detection result
+    plt.imshow(out_img_array, cmap='gray')
+    plt.title(f"Sobel Edge Detection (Threshold: {i})")
+    plt.axis('off')
+    plt.savefig(os.path.join(output_dir, f"2_Sobel_Edge_Detection_Threshold_{i}.png"))
+
+plt.figure()
+plt.plot(thres, detected_points, marker='o')
+plt.title("Number of Detected Edge Pixels vs. Threshold")
+plt.xlabel("Threshold Value")
+plt.ylabel("Number of Edge Pixels")
+plt.grid(True)
+plt.savefig(os.path.join(output_dir, "Sobel_Edge_Pixel_Count_vs_Threshold.png"))
 plt.show()
 
 # Log edge detection
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Digital Image Processing Project:
 - First Assignment: Variations of Histogram Equalization and Histogram Matching Algorithms
+
+  <img src="1st_assignment/output_plots/4a_reference_image_and_histogram.png" alt="Histogram" width="300"/>
+
+
 - Second Assignment: Edge and circle detection
+
+  <img src="2nd_assignment/images/Guernica_edge.png" alt="Guernica" width="300"/>
+
